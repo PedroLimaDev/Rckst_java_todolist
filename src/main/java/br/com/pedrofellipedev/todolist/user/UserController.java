14f1f5943db2d755d98f31c5aee3ebafa240d6a8
Pedro Fellipe <49622290+PedroLimaDev@users.noreply.github.com>
2023-10-14 16:14:27 -0300

    feat/AuthImplementation
    Initial Implementation of basic authorization for services

--- src/main/java/br/com/pedrofellipedev/todolist/user/UserController.java
+++ src/main/java/br/com/pedrofellipedev/todolist/user/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -21,8 +23,10 @@ public class UserController {
       return ResponseEntity.status(400).body("User already exists");
     }
 
+    var hashedPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(hashedPassword);
     var userCreated = this.IUserRepository.save(userModel);
 
-      return ResponseEntity.status(200).body(userCreated);
+    return ResponseEntity.status(200).body(userCreated);
   }
 }
